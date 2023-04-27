076612b6fbe59f8a2fd8464485df3396f012dadb
xoxlushka1337 <xoxlushka@gmail.com>
2023-04-27 13:15:39 +0300

    feat: add a field for text

---
+++ index.js
@@ -0,0 +1,8 @@
+const body = document.getElementsByTagName("body");
+
+document.body.innerHTML = `
+<div class="container">
+<textarea class="text" name="" id="" cols="30" rows="10"></textarea>
+<div class="keyboard"></div>
+</div>
+`;
