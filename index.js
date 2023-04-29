d2f4e1cfe2100c76f7c77111979d437c8de9ac37
xoxlushka1337 <xoxlushka@gmail.com>
2023-04-29 14:58:55 +0300

    feat: add ESLint and fix bugs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const body = document.getElementsByTagName("body");
+const body = document.querySelector('.body');
 
-document.body.innerHTML = `
+body.innerHTML = `
 <div class="container">
 <textarea class="text" name="" id="" cols="30" rows="10"></textarea>
 <div class="keyboard"></div>
@@ -14,87 +14,87 @@ const keyboard = [
   32, 18, 17, 37, 38, 40, 39,
 ];
 const letters = [
-  "`",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "0",
-  "-",
-  "=",
-  "Backspace",
-  "Tab",
-  "Q",
-  "W",
-  "E",
-  "R",
-  "T",
-  "Y",
-  "U",
-  "I",
-  "O",
-  "P",
-  "[",
-  "]",
-  "\\",
-  "CapsLock",
-  "A",
-  "S",
-  "D",
-  "F",
-  "G",
-  "H",
-  "J",
-  "K",
-  "L",
-  ";",
+  '`',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+  '-',
+  '=',
+  'Backspace',
+  'Tab',
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  '[',
+  ']',
+  '\\',
+  'CapsLock',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  ';',
   "'",
-  "Enter",
-  "Shift",
-  "Z",
-  "X",
-  "C",
-  "V",
-  "B",
-  "N",
-  "M",
-  ",",
-  ".",
-  "/",
-  "Shift",
-  "Control",
-  "Meta",
-  "Alt",
-  " ",
-  "Alt",
-  "Control",
-  "ArrowLeft",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowRight",
+  'Enter',
+  'Shift',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  ',',
+  '.',
+  '/',
+  'Shift',
+  'Control',
+  'Meta',
+  'Alt',
+  ' ',
+  'Alt',
+  'Control',
+  'ArrowLeft',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowRight',
 ];
 function init() {
-  let out = "";
+  let out = '';
 
-  for (let i = 0; i < letters.length; i++) {
-    if (letters[i] === "Backspace") {
+  for (let i = 0; i < letters.length; i += 1) {
+    if (letters[i] === 'Backspace') {
       out += ` <div class="keyboard__key keyboard__key_more" data="${keyboard[i]}">${letters[i]}</div>`;
-    } else if (letters[i] === "Tab") {
+    } else if (letters[i] === 'Tab') {
       out += `<div class="keyboard__key keyboard__key_row2"  data="${keyboard[i]}">Tab</div>`;
-    } else if (letters[i] === "CapsLock") {
+    } else if (letters[i] === 'CapsLock') {
       out += `<div class="keyboard__key keyboard__key_row3" data="${keyboard[i]}">Caps</div>`;
-    } else if (letters[i] === "Enter") {
+    } else if (letters[i] === 'Enter') {
       out += `<div class="keyboard__key keyboard__key_row3" data="${keyboard[i]}">Enter</div>`;
-    } else if (letters[i] === "Shift") {
+    } else if (letters[i] === 'Shift') {
       out += `<div class="keyboard__key keyboard__key_row4" data="${keyboard[i]}">Shift</div>`;
-    } else if (letters[i] === " ") {
+    } else if (letters[i] === ' ') {
       out += ` <div class="keyboard__key keyboard__key_space" data="${keyboard[i]}"></div>`;
-    } else if (letters[i] === "ArrowLeft") {
+    } else if (letters[i] === 'ArrowLeft') {
       out += `
       <div class="keyboard__key" data="${keyboard[i]}">
       <img
@@ -103,12 +103,12 @@ function init() {
         alt=""
       />
       </div>`;
-    } else if (letters[i] === "ArrowUp") {
+    } else if (letters[i] === 'ArrowUp') {
       out += `<div class="keyboard__arrow">
       <div class="keyboard__key keyboard__arrow_up" data="${keyboard[i]}">
       <img class="keyboard__pointer " src="./imgs/pointer.png" alt="" />
     </div>`;
-    } else if (letters[i] === "ArrowDown") {
+    } else if (letters[i] === 'ArrowDown') {
       out += `<div class="keyboard__key keyboard__arrow_down" data="${keyboard[i]}">
       <img
         class="keyboard__pointer keyboard__pointer_down"
@@ -117,7 +117,7 @@ function init() {
       />
     </div>
     </div>`;
-    } else if (letters[i] === "ArrowRight") {
+    } else if (letters[i] === 'ArrowRight') {
       out += `<div class="keyboard__key" data="${keyboard[i]}">
       <img
         class="keyboard__pointer keyboard__pointer_right"
@@ -130,18 +130,17 @@ function init() {
     }
   }
   document.querySelector(
-    ".keyboard"
+    '.keyboard',
   ).innerHTML = `<div class="keyboard__line">${out}</div>`;
 }
 init();
-document.onkeydown = function (e) {
-  console.log(e.keyCode);
+document.onkeydown = function makesKeyActive(e) {
   // keyboard.push(e.keyCode);
   // console.log(keyboard);
-  document.querySelectorAll(`.keyboard__key`).forEach(function (element) {
-    element.classList.remove("active");
+  document.querySelectorAll('.keyboard__key').forEach((element) => {
+    element.classList.remove('active');
   });
   document
     .querySelector(`.keyboard__key[data="${e.keyCode}"]`)
-    .classList.add("active");
+    .classList.add('active');
 };
