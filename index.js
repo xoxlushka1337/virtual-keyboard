d8f825d9d97b4a236fbe2b979a61cfcdbe53e625
xoxlushka1337 <xoxlushka@gmail.com>
2023-04-30 16:31:04 +0300

    fix: add del key and grid layout

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ body.innerHTML = `
 
 const keyboard = [
   192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8, 9, 81, 87, 69, 82,
-  84, 89, 85, 73, 79, 80, 219, 221, 220, 20, 65, 83, 68, 70, 71, 72, 74, 75, 76,
-  186, 222, 13, 16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 17, 91, 18,
-  32, 18, 17, 37, 38, 40, 39,
+  84, 89, 85, 73, 79, 80, 219, 221, 220, 46, 20, 65, 83, 68, 70, 71, 72, 74, 75,
+  76, 186, 222, 13, 16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 17, 91,
+  18, 32, 18, 17, 37, 38, 40, 39,
 ];
 const letters = [
   '`',
@@ -42,6 +42,7 @@ const letters = [
   '[',
   ']',
   '\\',
+  'Del',
   'CapsLock',
   'A',
   'S',
@@ -82,19 +83,7 @@ function init() {
   let out = '';
 
   for (let i = 0; i < letters.length; i += 1) {
-    if (letters[i] === 'Backspace') {
-      out += ` <div class="keyboard__key keyboard__key_more" data="${keyboard[i]}">${letters[i]}</div>`;
-    } else if (letters[i] === 'Tab') {
-      out += `<div class="keyboard__key keyboard__key_row2"  data="${keyboard[i]}">Tab</div>`;
-    } else if (letters[i] === 'CapsLock') {
-      out += `<div class="keyboard__key keyboard__key_row3" data="${keyboard[i]}">Caps</div>`;
-    } else if (letters[i] === 'Enter') {
-      out += `<div class="keyboard__key keyboard__key_row3" data="${keyboard[i]}">Enter</div>`;
-    } else if (letters[i] === 'Shift') {
-      out += `<div class="keyboard__key keyboard__key_row4" data="${keyboard[i]}">Shift</div>`;
-    } else if (letters[i] === ' ') {
-      out += ` <div class="keyboard__key keyboard__key_space" data="${keyboard[i]}"></div>`;
-    } else if (letters[i] === 'ArrowLeft') {
+    if (letters[i] === 'ArrowLeft') {
       out += `
       <div class="keyboard__key" data="${keyboard[i]}">
       <img
@@ -130,13 +119,13 @@ function init() {
     }
   }
   document.querySelector(
-    '.keyboard',
+    '.keyboard'
   ).innerHTML = `<div class="keyboard__line">${out}</div>`;
 }
 init();
 document.onkeydown = function makesKeyActive(e) {
   // keyboard.push(e.keyCode);
-  // console.log(keyboard);
+  console.log(e.keyCode);
   document.querySelectorAll('.keyboard__key').forEach((element) => {
     element.classList.remove('active');
   });
